0d59b6d97e2f4c14710eb6d99ef2de7a9f690bdf
ArkadiuszGarzel <ejkej87@gmail.com>
2019-02-19 21:10:15 +0100

    Change to jsx

--- components/Contact.js
+++ components/Contact.js
@@ -1,21 +1,18 @@
 var Contact = React.createClass({
-   propTypes: {
-      item: React.PropTypes.object.isRequired,
-   },
+  propTypes: {
+    item: React.PropTypes.object.isRequired
+  },
 
-   render: function() {
-      return (
-         React.createElement('div', {className: 'contactItem'},
-                             React.createElement('img', {
-            className: 'contactImage',
-            src: 'https://www.shareicon.net/download/2015/08/22/89359_contact_512x512.png'
-         }),
-                             React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-                             React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-                             React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-                                                 this.props.item.email
-                                                )
-                            )
-      )
-   },
-});
+  render: function () {
+    return (
+      <div className={'contactItem'}>
+        <img className={'contactImage'} src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK2f6bz4N1XJFmykHlXC8CmAlk1Q_NbI0WNDU5T5J-GeapeUYAog'} />
+        <p className={'contactLabel'}>Imię: {this.props.item.firstName}</p>
+        <p className={'contactLabel'}>
+          Nazwisko: {this.props.item.lastName}
+        </p>
+        <a href={'mailto:' + this.props.item.email}>{this.props.item.email}</a>
+      </div>
+    )
+  }
+})
